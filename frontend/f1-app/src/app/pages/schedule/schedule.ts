12bc4ee0a9525158b44db6bc8f91de957dc7973d
Sergio Damaso Guerra <alumno@ulpgc.es>
2026-08-26 19:14:39 +0100

    feat: week calendar added to schedule page

--- frontend/f1-app/src/app/pages/schedule/schedule.ts
+++ frontend/f1-app/src/app/pages/schedule/schedule.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { Event } from '../../models/event';
+import { CalendarWeek } from '../../models/event';
 import { EventsService } from '../../services/events.service';
 import { EventCard } from '../../components/event-card/event-card';
+import {CalendarService} from '../../services/calendar.service';
+import {WeekCalendar} from '../../components/week-calendar/week-calendar';
 
 @Component({
   selector: 'app-schedule',
-  imports: [EventCard],
+  imports: [EventCard, WeekCalendar],
   templateUrl: './schedule.html',
   styleUrl: './schedule.css',
   standalone: true,
 })
 export class Schedule implements OnInit {
   events = signal<Event[]>([]);
+  weeks: CalendarWeek[] = [];
 
-  constructor(private eventsService: EventsService) {}
+  constructor(
+    private eventsService: EventsService,
+    private calendarService: CalendarService,
+  ) {}
 
   ngOnInit() {
     this.eventsService.getEvents().subscribe((events: Event[]) => {
       this.events.set(events);
+      this.weeks = this.calendarService.generateWeeks(this.events());
       setTimeout(() => {
         this.scrollToRelevantEvent();
       });
@@ -30,13 +38,28 @@ export class Schedule implements OnInit {
       this.events().find((event) => event.status === 'upcoming');
 
     if (!target) {
-      console.log("no ha encontrado target")
+      console.log('no ha encontrado target');
       return;
     }
 
     const element = document.getElementById(`event-${target.id}`);
 
-    if(!element) {
+    if (!element) {
+      console.log('no ha encontrado elemento');
+    }
+
+    element?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+    });
+  }
+
+  scrollToEvent(event: Event): void {
+    if(!event) return;
+
+    const element = document.getElementById(`event-${event.id}`);
+
+    if (!element) {
       console.log('no ha encontrado elemento');
     }
 
